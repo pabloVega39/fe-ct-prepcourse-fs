cf5b6808276b9951cc229bd7db56aba4e8ed926a
pabloVega <pablovegaperez39@gmail.com>
2023-08-31 10:40:45 -0600

    Homework modulo 7 completada

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -10,18 +10,42 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
+   class Usuario {  
+      constructor(usuario, nombre, email, password){
+      this.usuario = usuario;
+      this.nombre = nombre;
+      this.email = email;
+      this.password = password;
+      }
+      saludar() {
+         return 'Hola, mi nombre es ' + this.nombre;
+      }
+   }
+   return Usuario;
 }
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+   Usuario.prototype.saludar = function(){
+      return 'Hello World!';
+   }
 }
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+   String.prototype.reverse = function() {
+      var desarmado = this.split('');
+      var reversedArr = [];
+      for(let i=0; i<desarmado.length; i++){
+         reversedArr.unshift(desarmado[i]);
+      }
+      var reversedString = reversedArr.join('');
+      return reversedString;
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
